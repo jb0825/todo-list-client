034e672e6c65c122c3f604c3fb2cd1c253d5c5a6
JB <jebi0825@naver.com>
2021-11-15 11:59:12 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Filter from "./components/Filter";
 import Todo from "./components/Todo";
 
 function App () {
-    // 커밋왜안됨? 커밋왜안됨? 커밋왜안됨?
-    // 커밋왜안됨? 커밋왜안됨? 커밋왜안됨?
     const [tasks, setTasks] = useState([]);
     const [taskFilter, setTaskFilter] = useState("All");
 
