bcf405c93de9727a42f1814e198fee35d468ea4a
HwangJubin <ghkdwnqls2281@gmail.com>
2021-11-15 11:50:12 +0900

    커밋!!!!!

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 export default function Filter (props) {
+    function handleClick (e) { props.updateFilter(e.target.textContent); }
+
     return (
         <>
-            <button type="button">All</button>
-            <button type="button">Active</button>
-            <button type="button">Completed</button>
+            <button type="button" onClick={handleClick}>All</button>
+            <button type="button" onClick={handleClick}>Active</button>
+            <button type="button" onClick={handleClick}>Completed</button>
         </>
     );
 }
